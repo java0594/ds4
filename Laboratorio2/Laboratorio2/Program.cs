5a1891de38b3917b7653a511d95c8766a3712225
java0594 <juanvasquez0594@gmail.com>
2025-09-15 19:39:45 -0500

    Laboratorio #2

---
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            //Ejemplo utilizando las variables de instacia de Clase.
+            client.FirstName = "Juan";
+            client.LastName = "Vasquez";
+            client.Age = 30;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+
+
+    public class Client
+    {
+        //Declarando variables de instancia de Clase.
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+
+        public string GetFullName()
+        {
+            //Utilizando variables de instancia dentro de metodos de la clase.
+            return FirstName + " " + LastName;
+        }
+    }
+
+}
---
+++ Laboratorio2/Laboratorio22/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laboratorio22
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Asignado valor a variable estatica.
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);
+        }
+    }
+    public class  MyClass
+    {
+        //Declarando variable estatica
+        public static int Valor;
+    }
+}
