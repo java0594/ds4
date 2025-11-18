5d5cfac09469afbf075d43909543573ebb2efcaf
java0594 <juanvasquez0594@gmail.com>
2025-11-17 20:18:52 -0500

    Laboratorio 15

---
+++ Laboratorio15/Laboratorio154/WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Laboratorio154.WebForm1" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Suma de dos números</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Número 1: <asp:TextBox ID="txtNumero1" runat="server"></asp:TextBox><br /><br />
+            Número 2: <asp:TextBox ID="txtNumero2" runat="server"></asp:TextBox><br /><br />
+            <asp:Button ID="btnSumar" runat="server" Text="Sumar" OnClick="btnSumar_Click" /><br /><br />
+            Resultado: <asp:Label ID="lblResultado" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Laboratorio15/Laboratorio154/WebForm1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Laboratorio154
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSumar_Click(object sender, EventArgs e)
+        {
+            int numero1, numero2;
+
+            bool esNumero1 = int.TryParse(txtNumero1.Text, out numero1);
+            bool esNumero2 = int.TryParse(txtNumero2.Text, out numero2);
+
+            if (esNumero1 && esNumero2)
+            {
+                int suma = numero1 + numero2;
+                lblResultado.Text = "La suma es: " + suma.ToString();
+            }
+            else
+            {
+                lblResultado.Text = "Por favor, ingrese números válidos.";
+            }
+        }
+    }
+}
